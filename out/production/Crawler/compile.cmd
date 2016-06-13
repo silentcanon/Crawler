2b74ba0a557007225c670092caea470fd84281ad
silentcanon <silentcanon@gmail.com>
2016-06-13 00:41:04 -0400

    ingore

--- out/production/Crawler/compile.cmd
+++
@@ -1,6 +0,0 @@
-echo "Start to compiling..."
-javac -Djava.ext.dirs=. com/canon/webcrawler/net/*.java
-javac -Djava.ext.dirs=. com/canon/webcrawler/utils/*.java
-javac -Djava.ext.dirs=. com/canon/webcrawler/crawler/*.java
-javac -Djava.ext.dirs=. com/canon/webcrawler/test/*.java
-echo "Succeed!"
