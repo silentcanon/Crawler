372b06c99eca6e98cd2fe1d99af4b735079ec931
silentcanon <silentcanon@gmail.com>
2016-06-13 00:57:07 -0400

    f

--- src/run.cmd
+++
@@ -1,5 +0,0 @@
-echo "Now start to crawling..."
-java -Djava.ext.dirs=.  com.canon.webcrawler.test.Main
-move *.json static\
-echo "Succeed! Please open index.html for visulization!"
-
--- src/run.sh
+++
@@ -1,5 +0,0 @@
-echo "Now start to crawling..."
-java -Djava.ext.dirs=.  com.canon.webcrawler.test.Main
-mv data.json dataWithGroup.json ./static
-echo "Succeed! Please open index.html for visulization!"
-
